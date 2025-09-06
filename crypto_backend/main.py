4a7efb80770de2ff4ed10810ed32c6cf2334a0e7
Ishan Ray Chaudhuri <ishan.rc2005@gmail.com>
2025-09-06 20:32:45 +0530

    Tweaking

--- crypto_backend/main.py
+++ crypto_backend/main.py
@@ -7,7 +7,7 @@ app = FastAPI()
 
 app.add_middleware(
     CORSMiddleware,
-    allow_origins=["https://coin-pulse-crypto-tracker.vercel.app"],  
+    allow_origins=["*"],
     allow_credentials=True,
     allow_methods=["*"],
     allow_headers=["*"],
